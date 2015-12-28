557aa2ab0529c5a6c1b007a44058fef3702e389c
taey16 <taey1600@gmail.com>
2015-12-28 20:05:42 +0900

    add predict.lua

---
+++ predict.lua
@@ -0,0 +1,113 @@
+require 'torch'
+require 'nn'
+require 'nngraph'
+require 'cutorch'
+require 'cunn'
+require 'cudnn'
+local utils = require '../misc.utils'
+require 'misc.DataLoader'
+require 'misc.DataLoaderRaw'
+require 'models.LanguageModel'
+local net_utils = require 'misc.net_utils'
+local cjson = require 'cjson'
+
+torch.setdefaulttensortype('torch.FloatTensor') -- for CPU
+
+local model_filename = 
+  '/storage/coco/checkpoints/_inception7_bs16_encode512/model_id_inception7_bs16_encode512.t7'
+  --'/storage/coco/checkpoints/_inception7_bs16_encode512_finetune_lr4e-6_clr1e-7_wc2e-5/model_id_inception7_bs16_encode512_finetune_lr4e-6_clr1e-7_wc2e-5.t7'
+local checkpoint = torch.load(model_filename)
+local batch_size = checkpoint.opt.batch_size
+local opt = {'rnn_size', 'input_encoding_size', 'drop_prob_lm', 'cnn_proto', 'cnn_model', 'seq_per_img', 'image_size', 'crop_size'}
+for k,v in pairs(opt) do 
+  opt[v] = checkpoint.opt[v]
+end
+local vocab = checkpoint.vocab
+local beam_size = 4
+
+local protos = checkpoint.protos
+protos.expander = nn.FeatExpander(opt.seq_per_img)
+protos.lm:createClones()
+for k,v in pairs(protos) do v:cuda() end
+protos.cnn:evaluate()
+protos.lm:evaluate()
+
+local json_filename = 
+  '/storage/coco/coco_raw.json'
+local file = io.open(json_filename, 'r')
+local text = file:read()
+file:close()
+local info = cjson.decode(text)
+
+local outfile = io.open(
+  --"coco_val2014.html", "w"
+  "model_id_inception7_bs16_encode512.t7.html", "w"
+)
+io.output(outfile)
+io.write("<html>\n  <head>\n    <table>\n      <tr>\n")
+
+--[[
+for k, v in pairs(info) do print(k) end
+annotations
+images
+info
+licenses
+--]]
+--[[
+for k, v in pairs(info['images']) do print(info['images'][k]) end
+{
+  file_name : "COCO_val2014_000000560744.jpg"
+  coco_url : "http://mscoco.org/images/560744"
+  flickr_url : "http://farm4.staticflickr.com/3791/9109408773_2235e8972e_z.jpg"
+  id : 560744
+  height : 480
+  license : 1
+  date_captured : "2013-11-25 15:04:29"
+  width : 640
+}
+--]]
+local iter = 1
+for k, v in ipairs(info) do
+  local fname = v['file_path'] 
+  local captions = v['captions']
+  print(fname)
+  local url = string.gsub(fname, '/storage', 'http://10.202.35.109:2596/PBrain')
+  io.write(string.format("        <td><img src=\"%s\" height=\"292\" width=\"292\"></br>\n", url))
+  for k,sent  in ipairs(captions) do
+    io.write(string.format("          <font color=\"blue\">%s</font></br>\n", sent))
+    print(sent)
+  end
+
+
+  local img = image.load(fname)
+  img = image.scale(img, 292, 292)
+  print(img:size())
+  if img:size(1) == 1 then
+    img = img:view(1,img:size(2), img:size(3)):repeatTensor(3,1,1)
+  end
+  img = net_utils.preprocess_inception7_predict(img, 256, false, 1)
+  print(img:size())
+  --print(img:type())
+  local data = torch.CudaTensor(2, 3, 256, 256):fill(0)
+  data[{{1},{},{},{}}] = img
+  local feats = protos.cnn:forward(data)
+  local sample_opts = 
+    { sample_max = opt.sample_max, beam_size = beam_size, temperature = opt.temperature }
+  local seq = protos.lm:sample(feats, sample_opts)
+  local sents = net_utils.decode_sequence(vocab, seq)
+  print('\n')
+  io.write(string.format("        <font color=\"green\">%s</font>", sents[1]))
+  io.write("      </td>\n")
+  if iter % 5 == 0 then
+    io.write("    </tr>\n<tr>\n")
+  end
+  for k,_ in pairs(sents) do
+    print(sents[k])
+  end
+  print('\n')
+  iter = iter+1
+end
+
+io.write("    </table>\n  </head>\n</html>")
+io.close(outfile)
+--]]
