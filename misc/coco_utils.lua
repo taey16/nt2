0ff77f172cdad45dd17cb287951a861a1bc750d5
taey16 <taey1600@gmail.com>
2016-01-06 13:17:53 +0900

    commit

--- misc/coco_utils.lua
+++ misc/coco_utils.lua
@@ -1,21 +1,37 @@
 
+local coco_utils = {}
 
-function get_val(filename_)
-  local filename = filename_ or '/storage/coco/test2015/'
-  local file = io.open filename
-  local image_list ={}
-  local label_list ={}
-  local synset_list={}
+function coco_utils.permute(tab, n, count)
+  n = n or #tab
+  for i = 1, count or n do
+    local j = math.random(i, n)
+    tab[i], tab[j] = tab[j], tab[i]
+  end
+  return tab
+end
+
+
+function coco_utils.get_test(filename_)
+  local filename = filename_ or '/storage/coco/test2015.txt'
+  local file = io.open(filename, 'r')
+  local filename_list ={}
+  local url_list ={}
+  local list_table = {}
 
   while true do
     local line = file:read()
     if not line then break end
-    local item  =string.split(line, ' ')
-    local synset=string.split(item[1], '/')
-    table.insert(image_list, item[1])
-    table.insert(label_list, item[2])
-    table.insert(synset_list,synset[1])
+    local item  =string.split(line, ',')
+    local fname = item[1]
+    local url = item[2]
+    table.insert(filename_list, fname)
+    table.insert(url_list, url)
+    list_table[fname] = url
   end
-  return image_list, label_list, synset_list
+
+  file:close()
+  return filename_list, url_list, list_table
 end
+
+return coco_utils
 
